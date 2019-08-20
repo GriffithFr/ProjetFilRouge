aa1dd72cb2891503cd2a914e700d25a9ab6b7cea
GriffithFr <mclosier@gmail.com>
2019-08-20 10:38:08 +0200

    Version 1

--- src/main/java/ProjetFilRouge/spring/SpringConfig.java
+++ src/main/java/ProjetFilRouge/spring/SpringConfig.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package streaming.spring;
+package ProjetFilRouge.spring;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -22,8 +22,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 @Configuration
 //@SpringBootApplication
 //@EnableAutoConfiguration
-@ComponentScan(basePackages = "streaming") 
-@EnableJpaRepositories(basePackages = "streaming")
+@ComponentScan(basePackages = "projetFilRouge") 
+@EnableJpaRepositories(basePackages = "projetFilRouge")
 @EnableScheduling
 public class SpringConfig {
 
@@ -36,7 +36,7 @@ public class SpringConfig {
 
     @Bean
     public EntityManagerFactory entityManagerFactory() {
-        return Persistence.createEntityManagerFactory("streaming_PU");
+        return Persistence.createEntityManagerFactory("PU");
     }
 
 }
