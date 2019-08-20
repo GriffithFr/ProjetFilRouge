9f766f5102c03527680a293bab9adaa6dc61190a
GriffithFr <mclosier@gmail.com>
2019-08-20 11:26:34 +0200

    Ajout table user

---
+++ src/main/java/filrouge/entity/Journaliste.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Projet_New;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author mclos
+ */
+@Entity
+public class Journaliste implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String journalisteMail;
+    @Column(nullable = false)
+    private String journalisteName;
+    @Column(nullable = false)
+    private String journalisteFirstName;
+    @Column(nullable = false)
+    private String journalistePassword;
+    private String journalistePhoto;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Journaliste)) {
+            return false;
+        }
+        Journaliste other = (Journaliste) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Projet_New.Journaliste[ id=" + id + " ]";
+    }
+
+}
