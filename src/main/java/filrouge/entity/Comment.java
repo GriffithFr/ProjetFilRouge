10598f434f33584e01705f8caca6dc69c7ada8c5
paul.dubois.64@gmail.com <pauld@DESKTOP-MJO85IH>
2019-08-20 14:18:21 +0200

    identity comment.java

--- src/main/java/filrouge/entity/Comment.java
+++ src/main/java/filrouge/entity/Comment.java
@@ -24,7 +24,7 @@ public class Comment implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 32, nullable = false)
     private String commentContain;
